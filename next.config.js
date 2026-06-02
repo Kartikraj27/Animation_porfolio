1a0d18d0b35b31eb0f07c2017788bb406d0a3b8e
Kartik Raj <142732460+Kartikraj27@users.noreply.github.com>
2026-06-02 10:25:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -9,6 +10,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
